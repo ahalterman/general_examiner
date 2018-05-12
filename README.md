cab6e4715ec7ae62a75ede25d5f55c0b6061441f
Andy <ahalterman0@gmail.com>
2018-05-12 11:36:05 -0400

    Add code to generate questions from tag

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # general_examiner
+
 Tools for studying for general exams
+
+## Extracting information from notes
+
+If notes are stored within a bibtex file's "annote" field using the format in
+`note_template.txt`, these scripts can extract and structure information
+contained in the fields. 
+
+
+`bib_to_questions.py` takes in the location of a .bib file and returns all the
+questions from the article notes matching that tag.
+
+## Example usage
+
+```
+ahalterman:general_examiner$ python bib_to_questions.py -i /Users/ahalterman/MIT/MIT.bib -t "Civil War"
+Extracting bibliographic entries from /Users/ahalterman/MIT/MIT.bib
+Bibliography has 1116 total entries.
+Found 15 entries for the tag 'Civil War'.
+1. What is the effect of aid on civil conflict? Does it increase or decrease?
+2. Does ethnic conflict cause civil war
+3. Do economic shocks increase or decrease the probability of civil war?
+4. Is there an ``opportunity cost'' mechanism in changing risk of war?
+...
+```
+
+
---
+++ bib_to_questions.py
@@ -0,0 +1,44 @@
+import bibtexparser
+from collections import Counter
+import re
+import genanki
+import plac
+
+@plac.annotations(
+    bib_file=("Location of .bib file", "option", "i", str),
+    tag=("Tag to extract questions for", "option", "t", str))
+def main(bib_file, tag):
+    print("Extracting bibliographic entries from {0}".format(bib_file))
+    with open(bib_file) as bibtex_file:
+        bibtex_str = bibtex_file.read()
+    bib_database = bibtexparser.loads(bibtex_str)
+    print("Bibliography has {0} total entries.".format(len(bib_database.entries)))
+
+    all_q = []
+
+    for i in bib_database.entries:
+        ts = []
+        if 'keywords' in i.keys():
+            # clean up the tags; some are comma and some are semicolon sep
+            ts = i['keywords'].replace(",", ";")
+            ts = ts.split(";")
+            ts = [i.strip() for i in ts]
+        if 'annote' in i.keys() and tag in ts:
+            questions = re.findall("Motivation\n(.+?)Argument", i['annote'], re.DOTALL)
+            if questions:
+                author = re.findall("^(.+?)\n", i['annote'], re.DOTALL)
+                if author:
+                    author = author[0].strip()
+                if author == "One Sentence Summary":
+                    print(i['ID'])
+                questions = re.sub("--------------------\n\n", "", questions[0]).strip()
+                if questions:
+                    questions = questions.split("\n")
+                    questions = [re.sub("^- ", "", i) for i in questions]
+                    all_q.extend(questions)
+    print("Found {0} entries for the tag '{1}'.".format(len(all_q), tag))
+    for n, i in enumerate(all_q):
+        print("{0}. {1}".format(n + 1, i))
+
+if __name__ == "__main__":
+    plac.call(main)
